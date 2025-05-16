ba7409474747278e02121ca0c0c1039cab7b4252
nadhanselvam11 <nadhankalarani@gmail.com>
2025-05-16 14:58:06 +0530

    Update Index.js

---
+++ Index.js
@@ -0,0 +1,31 @@
+setTimeout(() => {
+    document.getElementById("num").innerHTML = 1;
+    setTimeout(() => {
+        document.getElementById("num").innerHTML = 2;
+        setTimeout(() => {
+            document.getElementById("num").innerHTML = 3;
+            setTimeout(() => {
+                document.getElementById("num").innerHTML = 4;
+                setTimeout(() => {
+                    document.getElementById("num").innerHTML = 5;
+                    setTimeout(() => {
+                        document.getElementById("num").innerHTML = 6;
+                        setTimeout(() => {
+                            document.getElementById("num").innerHTML = 7;
+                            setTimeout(() => {
+                                document.getElementById("num").innerHTML = 8;
+                                setTimeout(() => {
+                                    document.getElementById("num").innerHTML = 9;
+                                    setTimeout(() => {
+                                        document.getElementById("num").innerHTML = "HAPPY DIWALI";
+                                  
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
